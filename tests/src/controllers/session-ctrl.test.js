95f050fa08221be7fb1dfc0afdfed9cfacd818c2
RubensLFerreira <rubenslima000@gmail.com>
2024-03-19 18:16:06 -0300

    test: criando testes para session-ctrl

--- tests/src/controllers/session-ctrl.test.js
+++ tests/src/controllers/session-ctrl.test.js
@@ -1,33 +1,86 @@
-const { faker } = require("@faker-js/faker");
+// const { faker } = require("@faker-js/faker");
 
-const sessionController = require("../../../src/controllers/session-ctrl");
-const emailValidator = require('../../../src/utils/email-validator');
-const userService = require('../../../src/services/user-service');
+const SessionController = require("../../../src/controllers/session-ctrl");
+const Email = require('../../../src/utils/email-validator');
+const UserService = require('../../../src/services/user-service');
+const SessionService = require('../../../src/services/session-service');
 
 const reqMock = {
   body: {
-    email: faker.internet.email(),
-    password: faker.internet.password(),
+    email: 'rubens@gmail.com',
+    password: 'senha123',
   },
 };
 
-const userServiceMock = {
-  create: {
-    email: faker.internet.email(),
-    password: faker.internet.password(),
+const resMock = {
+  status: () => {
+    return {
+      json: () => { }
+    }
   }
+};
+const userMock = {
+  email: 'rubens@gmail.com',
+  password: 'senha123',
+}
+
+const userServiceMock = {
+  create: async () => userMock
+}
+
+const sessionServiceMock = {
+  generateToken: async () => 'token'
 }
 
 describe("Testando o controller de sessão", () => {
-  test("Deve retornar um token ao autenticar um usuário", async () => {
-    jest.spyOn(emailValidator, 'isValid').mockImplementationOnce(() => true);
-
-    jest.spyOn(userService, 'userExistsAndCheckPassword').mockImplementationOnce(userServiceMock.create);
-    
-    
-    const result = await sessionController.create(reqMock)
-
-
-    console.log(result);
+  test('Deve retornar um erro 400 quando o email é inválido', async () => {
+    jest.spyOn(Email, 'isValid').mockImplementationOnce(() => false);
+    const resStatusSpy = jest.spyOn(resMock, 'status');
+    await SessionController.create(reqMock, resMock);
+    expect(resStatusSpy).toHaveBeenCalledWith(400);
   });
+
+  test('Deve retornar um erro 400 quando a senha é inválida ou ausente', async () => {
+    jest.spyOn(Email, 'isValid').mockImplementationOnce(() => true);
+
+    reqMock.body.password = '';
+    const resStatusSpy = jest.spyOn(resMock, 'status');
+    await SessionController.create(reqMock, resMock);
+    expect(resStatusSpy).toHaveBeenCalledWith(400);
+  });
+
+  test('Deve retornar um erro 404 quando o usuário não existe', async () => {
+    jest.spyOn(Email, 'isValid').mockImplementationOnce(() => true);
+    jest.spyOn(UserService, 'userExistsAndCheckPassword').mockImplementationOnce(() => {
+      throw { status: 404, message: 'Usuário não encontrado' };
+    });
+    const resStatusSpy = jest.spyOn(resMock, 'status');
+    await SessionController.create(reqMock, resMock);
+    expect(resStatusSpy).toHaveBeenCalledWith(404);
+  });
+
+  test('Deve retornar um erro 500 quando ocorrer um erro inesperado', async () => {
+    jest.spyOn(Email, 'isValid').mockImplementationOnce(() => true);
+    jest.spyOn(UserService, 'userExistsAndCheckPassword').mockImplementationOnce(() => {
+      throw new Error('Erro inesperado');
+    });
+
+    try {
+      await SessionController.create(reqMock, resMock);
+    } catch (error) {
+      expect(error.status).toBe(500);
+    }
+  });
+
+  // test('Deve retornar um token ao criar uma sessão', async () => {
+  //   jest.spyOn(Email, 'isValid').mockImplementationOnce(() => true);
+  //   jest.spyOn(UserService, 'userExistsAndCheckPassword').mockImplementationOnce(() => true);
+  //   jest.spyOn(SessionService, 'generateToken').mockImplementationOnce(() => 'token');
+  //   const resStatusSpy = jest.spyOn(resMock, 'status');
+  //   const resJsonSpy = jest.spyOn(resMock, 'status').mockReturnValue({ json: jest.fn() });
+
+  //   await SessionController.create(reqMock, resMock);
+  //   expect(resStatusSpy).toHaveBeenCalledWith(200);
+  //   expect(resJsonSpy).toHaveBeenCalledWith({ token: 'token' });
+  // });
 });
